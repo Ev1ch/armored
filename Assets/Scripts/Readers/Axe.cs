71fa9517dde844c5c2c691708855d4653ba51473
Vlad Zusko <vladzvu11@gmail.com>
2023-02-20 20:40:32 +0200

    feat: add axe enum

---
+++ Assets/Scripts/Readers/Axe.cs
@@ -0,0 +1,8 @@
+﻿namespace Readers
+{
+    public static class Axe
+    {
+        public static readonly string Horizontal = "Horizontal";
+        public static readonly string Vertical = "Vertical";
+    }
+}
