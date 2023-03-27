12285e51b54806ba088a22dd1c49b3c77be2794d
Vlad Zusko <vladzvu11@gmail.com>
2023-03-27 14:12:03 +0300

    feat: add level game initializer

---
+++ Assets/Scripts/Core/GameLevelInitializer.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using Players;
+using Readers;
+using Players.Abstracts;
+
+namespace Core
+{
+    public class GameLevelInitializer : MonoBehaviour
+    {
+        [SerializeField] private Player _player;
+        [SerializeField] private GameUIInputView _gameUIInputView;
+
+        private ExternalDevicesInputReader _externalDevicesInputReader;
+        private PlayerBrain _playerBrain;
+
+        private bool _onPause;
+
+        private void Awake()
+        {
+            _externalDevicesInputReader = new ExternalDevicesInputReader();
+            _playerBrain = new PlayerBrain(_player, new List<IEntityInputSource>()
+            {
+                _gameUIInputView,
+                _externalDevicesInputReader,
+            });
+        }
+
+        private void Update()
+        {
+            if (_onPause)
+            {
+                return;
+            }
+
+            _externalDevicesInputReader.OnUpdate();
+        }
+
+        private void FixedUpdate()
+        {
+            _playerBrain.OnFixedUpdate();
+        }
+    }
+}
